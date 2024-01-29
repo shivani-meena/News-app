d944749a3c83414dc2a2c86d6078947d27c21502
shivani-meena <shivanimeena21@navgurukul.org>
2024-01-29 22:46:36 +0530

    Updated the code for fetch data using useEffect

--- app/app/page.js
+++ app/app/page.js
@@ -5,36 +5,23 @@ import Search from "./searchPage";
 import { Article } from "./dummyData.js";
 import { useEffect, useState } from "react";
 function Home() {
-  const [articles, setArticles] = useState([]);
+  const [article, setArticle] = useState([]);
+
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=6f88d61a316c423786b8a37c81deaf33');
-
-        if (response.ok) {
-          const data = await response.json();
-          const articlesArray = data.articles || [];
-          console.log(data)
-
-          if (Array.isArray(articlesArray)) {
-            setArticles(articlesArray);
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+    fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=6f88d61a316c423786b8a37c81deaf33")
+    .then(response => response.json())
+    .then(data => setArticle(data.articles))
+  },[])
 
   return (
     <>
-      <Search />
+      <Search 
+      />
       <div className="container">
         <div className="text">TOP NEWS FROM INDIA</div>
         <div className="article row">
-          {articles.map((news) => (
+          {article.map((news) => (
             <div className="col-lg-4">
               <Newsrow
                 key={news.title}
@@ -48,5 +35,3 @@ function Home() {
   );
 };
 export default Home;
-
-
--- app/app/searchPage.js
+++ app/app/searchPage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 function Search() {
-    const [input, setInput] = useState([]);
+    const [input, setInput] = useState("");
+    const [search, setSearch] = useState([]);
 
     function handleSearch() {
         let data = fetch(`https://newsapi.org/v2/top-headlines?apiKey=6f88d61a316c423786b8a37c81deaf33&q=${input}`)
@@ -9,14 +10,29 @@ function Search() {
             return response1.json()
         }).then((response2) => {
             console.log(response2)
+            const value = response2.articles
+
+            if (Array.isArray(value)) {
+                setSearch(value);
+            }
         })
     }
-
     return (
         <>
+            <div className="row">
+                {search.map((news) => (
+                    <div className="col-lg-4" key={news.title}>
+                        <div>
+                            <h5>{news.title}</h5>
+                            <div>{news.description}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
             <div className="search">
                 <img className="iconSearch" src="search.png" />
                 <input type="search"
+                    placeholder="Search"
                     value={input}
                     onChange={(e) => setInput(e.target.value)}>
                 </input>
